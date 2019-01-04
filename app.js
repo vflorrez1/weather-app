5e799b34cdb89ab0e6c60549ee7df0b75382ed84
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-04 22:18:21 +0000

    added gitignore and dynamic api calls with address from the console

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request =  require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv; 
+
+    var encode = encodeURIComponent(argv.address)
+    
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=aKnKUlaeDy5MgRdHpfPwxb43cGIG75mz&location=1301%20lombard%20street%20philadelphia',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=aKnKUlaeDy5MgRdHpfPwxb43cGIG75mz&location=${encode}`,
     json: true
 }, (error, response, body) =>{
     console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
