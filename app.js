718622e9c42ddf68b8317a05eedae8d35d9ab84e
Victor <Victor@Victors-MacBook-Pro.local>
2019-01-05 12:18:53 +0000

    moving logic to geocode and calling weather from forcastIO api

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-const request =  require('request');
-const yargs = require('yargs');
 
-const argv = yargs
+var request = require('request');
+// const yargs = require('yargs');
 
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv; 
+// const geocode = require('./geocode/geocode')
 
-    var encode = encodeURIComponent(argv.address)
-    
+// const argv = yargs
+
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv; 
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+
 
 request({
-    url: `http://www.mapquestapi.com/geocoding/v1/address?key=aKnKUlaeDy5MgRdHpfPwxb43cGIG75mz&location=${encode}`,
+    url: 'https://api.darksky.net/forecast/cd729a0a67f153c54b1967c132467101/39.936521,-75.15489',
     json: true
-}, (error, response, body) =>{
-    if (error) {
-        console.log('unable to connect to server')
+}, (error, response, body) => {
+    if(!error && response.statusCode === 200) {
+        console.log(body.currently.temperature)
     } else {
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
-    }   
-});
+        console.log(`Unable to fetch weather. Error: ${response.statusCode}.`)
+    }
+})
+
+
+
---
+++ geocode/geocode.js
@@ -0,0 +1,25 @@
+const request =  require('request');
+
+
+var geocodeAddress = (address, callback) => {
+    var encode = encodeURIComponent(address)
+    
+
+    request({
+        url: `http://www.mapquestapi.com/geocoding/v1/address?key=aKnKUlaeDy5MgRdHpfPwxb43cGIG75mz&location=${encode}`,
+        json: true
+    }, (error, response, body) =>{
+        if (error) {
+            callback('unable to connect to server');
+        } else {
+        callback(undefined, {
+            latitude: body.results[0].locations[0].latLng.lat,
+            longitude: body.results[0].locations[0].latLng.lng
+         });
+        }   
+    });
+};
+
+module.exports = {
+    geocodeAddress
+};
